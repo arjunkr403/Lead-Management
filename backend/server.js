f7be17de3426fa571aae60175ccbaa0ba631cce4
Hzard <arjunkr403@gmail.com>
2026-01-16 19:05:00 +0530

    changed cors settings

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.get("/api/health", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
